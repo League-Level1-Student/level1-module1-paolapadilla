83645fcdff70f76ac01ce6cf0e356f6270aa6e5a
root <root@bccd26a45a61>
2020-05-16 18:27:26 +0000

    Working on binary convertor

---
+++ src/_07_binary_converter/BinaryConverter.java
@@ -0,0 +1,42 @@
+package _07_binary_converter;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class BinaryConverter {
+
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		// Create JTextField
+		// Add JTextField on panel
+		frame.add(panel);
+		frame.setVisible(true);
+		frame.setSize(400, 400);
+		
+		
+
+	}
+     
+	public String convert(String input) {
+		if (input.length() != 8) {
+			JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s,silly!!!");
+			return "-";
+		}
+		String binary = "[0-1]+";// must contain numbers in the given range
+		if (!input.matches(binary)) {
+			JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+			return "-";
+		}
+		try {
+			int asciiValue = Integer.parseInt(input, 2);
+			char theLetter = (char) asciiValue;
+			return "" + theLetter;
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+			return "-";
+		}
+	}
+}
